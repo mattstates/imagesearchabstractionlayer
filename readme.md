3310918c75b7d54e6294e0a6ada891f197b8ee95
Matt States <mattstates@icloud.com>
2017-05-14 22:46:14 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Image Search Abstraction Layer – freecodecamp
+
+This app uses a Google Custom Search to return image information which includes the image link, the site it is hosted on, and the image title.
+
+This app was built with
+
+  - node.js
+  - express.js
+
+Check out the app and documentation here, [https://img-search-abs-layer-fcc.herokuapp.com/](https://img-search-abs-layer-fcc.herokuapp.com/)
+
+Learn more at [freecodecamp](https://www.freecodecamp.com/challenges/image-search-abstraction-layer).
---
+++ server.js
@@ -0,0 +1,62 @@
+require('dotenv').config(); // process.env.VARNAME
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 7777;
+const path = require('path')
+
+const google = require('googleapis');
+const customsearch = google.customsearch('v1');
+let recentSearches = [];
+
+///// Middleware /////
+app.use(express.static(path.join(__dirname, 'public')));
+
+///// Routes /////
+
+// Home Page //
+app.get('/', (req, res) => {
+    res.send('I am a little teapot.')
+});
+
+// Recent Searches //
+app.get('/recent', (req, res) => {
+    res.json({
+        numberOfSearchesSinceAppRestart: recentSearches.length,
+        recentSearchTerms: recentSearches
+    })
+})
+
+// Image Search //
+app.get('/api/:searchTerm', (req, res) => {
+    const API_KEY = process.env.GOOGLE_API_KEY;
+    const CX = process.env.GOOGLE_CX;
+    const SEARCH = req.params.searchTerm;
+    const offset = !!req.query.offset ? req.query.offset * 10 : 10;
+
+    recentSearches.push(SEARCH);
+    // https://github.com/google/google-api-nodejs-client/tree/master/samples/customsearch
+    customsearch.cse.list({ cx: CX, q: SEARCH, auth: API_KEY, searchType: 'image', highRange: offset}, function (err, resp) {
+        if (err) {
+            return console.log('An error occured', err);
+        }
+
+        if (resp.items && resp.items.length > 0) {
+
+            const imageInfo = resp.items.map( (item) => {
+                return {
+                    title: item.title,
+                    link: item.link,
+                    displayLink: item.displayLink
+                }
+            });
+
+            res.json(imageInfo);
+        }
+    });
+})
+
+///// Server Start /////
+app.listen(port, function () {
+    console.log('listening on port ', port)
+})
